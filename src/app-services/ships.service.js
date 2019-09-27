885a35c05b29fac3ff7a9e89bbd0e8c01e371898
nzabala <nicolas.zabala@ext.aqualogybs.net>
2019-09-27 11:15:00 +0200

    Primer commit: lanzamos antes de comenzar con los requisitos de la app, un standard validate fix, para eliminar errores de código y pequeños cambios en los formatos de literales, y definiciones.
    Ademas añado a mi entorno de desarrollo, el estandar de angularJs.
    
    Signed-off-by: Nicolas Zabala Nicolas.ZABALA@ingenico.com

---
+++ src/app-services/ships.service.js
@@ -0,0 +1,26 @@
+(function () {
+  'use strict'
+
+  angular
+    .module('app')
+    .factory('ShipsService', ShipsService)
+
+  ShipsService.$inject = ['$http']
+  function ShipsService ($http) {
+    var service = { GetStarships: GetStarships }
+    return service
+
+    function GetStarships (url) {
+      if (!url) {
+        url = 'https://swapi.co/api/starships/'
+      }
+      return $http.get(url, {
+        headers: {
+          'Authorization': 'none'
+        }
+      }).then(function (res) {
+        return res.data
+      })
+    }
+  }
+})()
